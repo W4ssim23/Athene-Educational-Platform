763553be0ea99d13c939b5150be3fb5a5ed1179b
W4ssim23 <o_zouiten@estin.dz>
2024-07-27 18:28:29 +0100

    .

--- src/app/classes/TeacherClasses.jsx
+++ src/app/classes/TeacherClasses.jsx
@@ -34,7 +34,7 @@ function TeacherClasses({ claSs, user }) {
     fetchClasses();
   }, []);
 
-  if (loading) return <div>Loading...</div>;
+  if (loading) return <GradesSkeleton />;
 
   return (
     <div className="min-h-[80vh] flex flex-col gap-5">
@@ -93,3 +93,46 @@ function TeacherClasses({ claSs, user }) {
 }
 
 export default TeacherClasses;
+
+const GradesSkeleton = () => {
+  return (
+    <main className="flex min-h-screen flex-col gap-12 animate-pulse pt-24">
+      <div className="flex  gap-4 w-full flex-wrap">
+        {[...Array(7)].map((_, index) => (
+          <div
+            key={index}
+            className="p-2 flex flex-col justify-center items-center text-center font-poppins rounded-3xl shadow-md
+                relative overflow-hidden cursor-pointer 
+                w-[17%] min-h-[120px] sm:min-w-[170px] min-w-[150px]
+                transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-105 duration-300 
+                bg-opacity-30 bg-gray-400"
+          ></div>
+        ))}
+      </div>
+      <div className="flex  gap-4 flex-wrap">
+        {[...Array(3)].map((_, index) => (
+          <div
+            key={index}
+            className="p-2 flex flex-col justify-center items-center text-center font-poppins rounded-3xl shadow-md
+                relative overflow-hidden cursor-pointer 
+                w-[17%] min-h-[120px] sm:min-w-[170px] min-w-[150px]
+                transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-105 duration-300 
+                bg-opacity-30 bg-gray-400"
+          ></div>
+        ))}
+      </div>
+      <div className="flex  gap-4 flex-wrap">
+        {[...Array(5)].map((_, index) => (
+          <div
+            key={index}
+            className="p-2 flex flex-col justify-center items-center text-center font-poppins rounded-3xl shadow-md
+                relative overflow-hidden cursor-pointer 
+                w-[17%] min-h-[120px] sm:min-w-[170px] min-w-[150px]
+                transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-105 duration-300 
+                bg-opacity-30 bg-gray-400"
+          ></div>
+        ))}
+      </div>
+    </main>
+  );
+};
